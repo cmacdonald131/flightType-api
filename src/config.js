08242228a24231b9173ed5f53602f07a1bb27324
Christopher MacDonald <cmacdonald131@gmail.com>
2020-03-27 22:36:32 -0400

    heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://fqblaunotmipvk:374f7a7fd099717b4e2c16145668a3f114a4bf2c429d9cedf799d9b2fa152f0a@ec2-34-192-30-15.compute-1.amazonaws.com:5432/dea42l80vtgub0',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://nbvldrdbljpmaw:79aae87680a34d88a0171dcca59bc10f7ac002322f0a5711e35b71fb926b4a60@ec2-34-206-252-187.compute-1.amazonaws.com:5432/dfqnhfhq9ihkbn',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '50d',
 }
